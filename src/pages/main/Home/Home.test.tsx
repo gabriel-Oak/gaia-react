0507006b9d3e2337fe22f555082a56df47ae87bc
gabriel-Oak <gabriel.costa41@fatec.sp.gov.br>
2019-12-08 21:03:12 -0300

    mais alguns testes

--- src/pages/main/Home/Home.test.tsx
+++ src/pages/main/Home/Home.test.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import { createStore, applyMiddleware } from 'redux';
 import reducers from '../../../reducers';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import HomePage from './HomePage';
+import HomeForm from './HomeForm';
+import { menusMock } from '../../../shared/testMocks/menus';
 
 describe('NotFound', () => {
   let store: any;
@@ -19,12 +21,25 @@ describe('NotFound', () => {
     );
   });
 
-  it('Should render and match snapshot', () => {
+  it('Should render', () => {
     const div = document.createElement('div');
     ReactDOM.render(
       <Provider store={store}>
         <HomePage />
       </Provider>, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  describe('Home form', () => {
+    it('should match snapshot', () => {
+      const onSubmit = jest.fn(() => { });
+
+      const form = shallow(
+        <Provider store={store}>
+          <HomeForm onSubmit={onSubmit} menus={menusMock} />
+        </Provider>
+      );
+      expect(form).toMatchSnapshot();
+    });
   });
 });
---
+++ src/shared/testMocks/menus.ts
@@ -0,0 +1,62 @@
+export const menusMock = [
+  {
+    data: '2019-11-04T22:16:00.000Z',
+    guarnicao1: 'Batata Frita',
+    guarnicao2: 'Ameixa da jamaica',
+    opcao1: 'Omelete',
+    opcao2: 'Bife',
+    pratoPrincipal: 'Bacon',
+    salada: 'Alface e tomate',
+    sobremesa: 'Maça',
+    __v: 0,
+    _id: '5d08c85f4d6ea30b43bf6d3b'
+  },
+  {
+    data: '2019-11-05T22:16:00.000Z',
+    guarnicao1: 'Batata Frita',
+    guarnicao2: 'Ameixa da jamaica',
+    opcao1: 'Omelete',
+    opcao2: 'Bife',
+    pratoPrincipal: 'Bacon',
+    salada: 'Alface e tomate',
+    sobremesa: 'Maça',
+    __v: 0,
+    _id: '5d08c85f4d6ea30b43bf6d3b'
+  },
+  {
+    data: '2019-11-06T22:16:00.000Z',
+    guarnicao1: 'Batata Frita',
+    guarnicao2: 'Ameixa da jamaica',
+    opcao1: 'Omelete',
+    opcao2: 'Bife',
+    pratoPrincipal: 'Bacon',
+    salada: 'Alface e tomate',
+    sobremesa: 'Maça',
+    __v: 0,
+    _id: '5d08c85f4d6ea30b43bf6d3b'
+  },
+  {
+    data: '2019-11-07T22:16:00.000Z',
+    guarnicao1: 'Batata Frita',
+    guarnicao2: 'Ameixa da jamaica',
+    opcao1: 'Omelete',
+    opcao2: 'Bife',
+    pratoPrincipal: 'Bacon',
+    salada: 'Alface e tomate',
+    sobremesa: 'Maça',
+    __v: 0,
+    _id: '5d08c85f4d6ea30b43bf6d3b'
+  },
+  {
+    data: '2019-11-08T22:16:00.000Z',
+    guarnicao1: 'Batata Frita',
+    guarnicao2: 'Ameixa da jamaica',
+    opcao1: 'Omelete',
+    opcao2: 'Bife',
+    pratoPrincipal: 'Bacon',
+    salada: 'Alface e tomate',
+    sobremesa: 'Maça',
+    __v: 0,
+    _id: '5d08c85f4d6ea30b43bf6d3b'
+  },
+]
